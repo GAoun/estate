8a7784a27095b3a04bd532d8be2760cfbe5b5c34
georges.oun@gmail.com <georges.oun@gmail.com>
2023-10-21 18:32:25 +0300

    update user api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { get_users } from '../controllers/user.controller.js';
+import { get_users, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/get_users', get_users);
+router.post('/update/:id', verifyToken, updateUser);
 
 export default router;
