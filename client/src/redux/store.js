9cdc4ce4840370d3bcd8f95c7a0ff41d3e396d4d
georges.oun@gmail.com <georges.oun@gmail.com>
2023-10-21 16:49:21 +0300

    add redux toolkit

---
+++ client/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import userReducer from './User/userSlice';
+
+export const store = configureStore({
+    reducer: { user: userReducer },
+    middleware: (getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: false
+    }))
+});
